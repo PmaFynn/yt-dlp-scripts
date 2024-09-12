f3502631160607f0217c85b1f966a4239bfacfa6
fynn <fynn091209@proton.me>
2024-09-12 11:10:29 +0200

    idk 2

--- scripts/Audio-Only Scripts/Archivist Scripts/Channels/Channels.sh
+++ scripts/Audio-Only Scripts/Archivist Scripts/Channels/Channels.sh
@@ -7,4 +7,4 @@
 # Version: 3.4.0
 #
 
-yt-dlp --format "(bestaudio[acodec^=opus]/bestaudio)/best" --verbose --force-ipv4 --sleep-requests 1 --sleep-interval 5 --max-sleep-interval 30 --ignore-errors --no-continue --no-overwrites --download-archive archive.log --add-metadata --parse-metadata "%(title)s:%(meta_title)s" --parse-metadata "%(uploader)s:%(meta_artist)s" --embed-thumbnail --extract-audio --check-formats --concurrent-fragments 3 --match-filter "!is_live & !live" --output "%(uploader)s/%(title)s.%(ext)s" --throttled-rate 100K --batch-file "Source - Channels.txt" 2>&1 | tee output.log
+yt-dlp --format "(bestaudio[acodec^=opus]/bestaudio)/best" --audio-format m4a --verbose --force-ipv4 --sleep-requests 1 --sleep-interval 5 --max-sleep-interval 30 --ignore-errors --no-continue --no-overwrites --download-archive archive.log --add-metadata --parse-metadata "%(title)s:%(meta_title)s" --parse-metadata "%(uploader)s:%(meta_artist)s" --embed-thumbnail --extract-audio --check-formats --concurrent-fragments 3 --match-filter "!is_live & !live" --output "%(uploader)s/%(title)s.%(ext)s" --throttled-rate 100K --batch-file "Source - Channels.txt" 2>&1 | tee output.log
